f5ab7839705b5362a4e7af3068c01e3ec9f6070e
jambun <jambun@gmail.com>
2015-06-23 12:03:33 -0400

    adding data from archival_object

--- schemas/top_container.rb
+++ schemas/top_container.rb
@@ -52,6 +52,27 @@
         }
       },
 
+      "linked_records" => {
+        "readonly" => "true",
+        "type" => "array",
+        "items" => {
+          "type" => "object",
+          "subtype" => "ref",
+          "properties" => {
+            "ref" => {
+              "type" => "JSONModel(:archival_object) uri",
+            },
+            "display_string" => {"type" => "string"},
+            "identifier" => {"type" => "string"},
+            "level_display_string" => {"type" => "string"},
+            "_resolved" => {
+              "type" => "object",
+              "readonly" => "true"
+            }
+          }
+        }
+      },
+
       "series" => {
         "readonly" => "true",
         "type" => "array",
