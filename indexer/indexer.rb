5439b4fe39cc2c142c56eb2ec88d341ed585d61d
jambun <jambun@gmail.com>
2015-10-29 13:31:40 +1100

    add indicator to the top container search

--- indexer/indexer.rb
+++ indexer/indexer.rb
@@ -48,6 +48,8 @@ class CommonIndexer
         doc['empty_u_sbool'] = record['record']['collection'].empty?
 
         doc['typeahead_sort_key_u_sort'] = record['record']['indicator'].to_s.rjust(255, '#')
+
+        doc['indicator_u_sstr'] = record['record']['indicator']
       end
     }
 
